622641b1b7bbe11413e92bc5e912a1f119443f89
Roohiya <winspo_dev_91@Roohiyas-MacBook-Pro-3.local>
2019-01-22 17:05:51 +0800

    Add part of day 1

---
+++ day1/frequency-part2.js
@@ -0,0 +1,15 @@
+const fs = require('fs')
+
+let input = fs.readFileSync('./input.txt', 'UTF-8')
+
+let inputArr = input.split("\n")
+
+console.log(inputArr)
+
+let sumArr = []
+let currentSum = 0
+let i = 0
+
+while(!sumArr[currentSum]) {
+    sum
+}
